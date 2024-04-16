fbcff25798453d4a5007d3569ca779218965045e
mahaomeng <mahaomeng@softsugar.com>
2024-04-16 10:36:37 +0800

    Initial Commit

---
+++ TakeItEasy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TakeItEasy
+//
+//  Created by 马浩萌 on 2024/4/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TakeItEasy/TakeItEasyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeItEasyApp.swift
+//  TakeItEasy
+//
+//  Created by 马浩萌 on 2024/4/16.
+//
+
+import SwiftUI
+
+@main
+struct TakeItEasyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
